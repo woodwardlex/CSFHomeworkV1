cbd5e0c35e67fa21f9a9cea81bf52ca7d0df4ba0
Alexis Woodward <alexis.woodward1717@outlook.com>
2020-10-05 12:51:22 -0500

    completed MAdLib

---
+++ CSF1HomewordV1/MadLib.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSF1HomewordV1
+{
+    class MadLib
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Mad Lib\n");
+            Console.Write("Give a noun: ");
+            string noun1 = Console.ReadLine();
+
+            Console.Write("Give a verb: ");
+            string verb1 = Console.ReadLine();
+
+            Console.Write("Give an adjective: ");
+            string adjective1 = Console.ReadLine();
+
+            Console.Write("Give another noun: ");
+            string noun2 = Console.ReadLine();
+
+            Console.Write("Give another verb: ");
+            string verb2 = Console.ReadLine();
+
+            Console.Write("Give another ajective: ");
+            string adjective2 = Console.ReadLine();
+
+            Console.WriteLine($"The {noun1} had {verb1} a {adjective1} {noun2} in the {verb2} of the {adjective2} castle.");
+        }
+    }
+}
